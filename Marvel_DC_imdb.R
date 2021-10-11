6892de9c18a12853b25cbf7aa057cee988a7b48b
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2021-10-11 15:32:54 +0700

    นำ Dataset เข้า R

--- Marvel_DC_imdb.R
+++ Marvel_DC_imdb.R
@@ -2,3 +2,8 @@
 library(readr)
 library(assertive)
 library(stringr)
+library(dplyr)
+
+read_csv("C:/Users/user/Desktop/int214/Marvel_DC_imdb.csv")
+Marvel_DC <- read_csv("C:/Users/user/Desktop/int214/Marvel_DC_imdb.csv")
+View(Marvel_DC)
