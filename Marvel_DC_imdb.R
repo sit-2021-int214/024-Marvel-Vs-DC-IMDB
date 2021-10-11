a8cfe2078dc01d9ac01c4bd5d4e951988d9c4614
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2021-10-11 15:33:45 +0700

    Update Marvel_DC_imdb.R

--- Marvel_DC_imdb.R
+++ Marvel_DC_imdb.R
@@ -4,6 +4,7 @@ library(assertive)
 library(stringr)
 library(dplyr)
 
+## Dataset
 read_csv("C:/Users/user/Desktop/int214/Marvel_DC_imdb.csv")
 Marvel_DC <- read_csv("C:/Users/user/Desktop/int214/Marvel_DC_imdb.csv")
 View(Marvel_DC)
