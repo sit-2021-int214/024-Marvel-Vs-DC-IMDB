fa7c85dbc783b6aa24c227632fa97ccfbfb09619
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2021-10-11 23:55:09 +0700

    update Remove () , text ใน Year

--- Marvel_DC_imdb.R
+++ Marvel_DC_imdb.R
@@ -11,3 +11,8 @@ View(Marvel_DC)
 
 # Remove แต่ละ col ที่ต้องการจะใช้งานที่มีข้อมูล Na
 Marvel_DC <- Marvel_DC %>% filter(!is.na(Marvel_DC$Rating))
+
+#Remove () , text ใน Year
+Marvel_DC$Year %>% str_remove("[()]") %>% str_remove("[()]") %>% str_remove("TV Special")%>% 
+  str_remove("Video")%>% str_remove("Video Game")%>% str_remove("Game")%>% str_remove(" TV Movie")%>% str_remove("III")%>% 
+  str_trim()%>% str_remove("[()]") %>% str_remove("[()]")
